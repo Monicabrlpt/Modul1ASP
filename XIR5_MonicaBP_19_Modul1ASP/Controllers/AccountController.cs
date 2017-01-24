d1f2248c0c631a2e65b4e90634c00add386bdd11
Monicabrlpt <monicabrlpt@gmail.com>
2017-01-23 17:42:13 -1200

    Add project files.

---
+++ XIR5_MonicaBP_19_Modul1ASP/Controllers/AccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using XIR5_MonicaBP_19_Modul1ASP.Models.ViewModel;
+using XIR5_MonicaBP_19_Modul1ASP.Models.EntityManager;
+
+namespace XIR5_MonicaBP_19_Modul1ASP.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult SignUp(UserSignUpView USV)
+        {
+            if (ModelState.IsValid)
+            {
+                UserManager UM = new UserManager();
+            if (!UM.IsLoginNameExist(USV.LoginName))
+                {
+                    UM.AddUserAccount(USV);
+                    FormsAuthentication.SetAuthCookie(USV.FirstName, false);
+                    return RedirectToAction("Welcome", "Home");
+                }
+            else
+                    ModelState.AddModelError("", "Login Name already taken.");
+            }
+            return View();
+        }
+    }
+}
---
+++ XIR5_MonicaBP_19_Modul1ASP/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XIR5_MonicaBP_19_Modul1ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
